f11d1085d176b530282141137b4e309cb13f875e
Youfka <mariyakat@yandex.ru>
2019-05-03 19:14:45 +0300

    check user JSON

---
+++ src/components/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <textarea name="" id="" cols="30" rows="10" @change="getJSON($event)" value=""></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GetUserJSON',
+  props: {
+    msg: String
+  },
+  methods: {
+    getJSON(ev){
+      console.log(JSON.parse(ev.target.value), ev.target.value)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
