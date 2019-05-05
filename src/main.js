ee48210a70f4981c1fd83fcd40b19efed18d8695
Youfka <mariyakat@yandex.ru>
2019-05-05 20:47:33 +0300

    remove that shitty framework and leave vanilla js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,48 @@
-import Vue from 'vue'
-import App from './App.vue'
+function setError(err) {
+  document.querySelector('.js-error').innerHTML = err;
+}
 
-Vue.config.productionTip = false
+function UserException() {
+  setError("Значение должно быть объектом или массивом!");
+}
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+function getJSON(ev) {
+  let value = ev.replace(/'/gi, '"');
+  try {
+    value = JSON.parse(value);
+    if(typeof value !== 'object') {
+      try { throw new UserException(); } 
+      catch(e){
+        console.log(e);
+      }
+    } 
+    setError('');
+  } catch(err){
+    try { throw new UserException(); }
+    catch(e){
+      console.log(e);
+    }
+  }
+  checkJSON(value);
+}
+
+function checkJSON(value){
+  if(Array.isArray(value)){
+    writeTests(true);
+  }
+}
+
+function writeTests(array){
+  let test;
+  if(array){
+    test = `cy.request('GET', 'http://test.site/test').then((response) => {
+      expect(response.status).to.eq(200)
+      cy.wrap(response.body).its('length').should('be.gt', 1)
+      cy.wrap(response.body).should('be.a', 'Array')
+      cy.wrap(response.body).each((value)=>{
+        cy.wrap(value).should('be.a', 'Object')
+      })
+    })`;
+  }
+  return test;
+}
